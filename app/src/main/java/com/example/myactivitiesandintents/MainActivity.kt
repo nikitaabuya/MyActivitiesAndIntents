622d8336821013583b6ea03d67eb08fc9b8b4896
nikitaabuya <kittaabuya@gmail.com>
2024-10-15 23:39:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myactivitiesandintents/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myactivitiesandintents
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+        val button = findViewById<Button>(R.id.button_to_second_activity)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+        }
+
+
+        supportActionBar?.title = "Activities & Intents - I"
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+}
